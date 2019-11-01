6e94af0534030891c520980629f4557d6514d813
Captaincrogs <2003829@talnet.nl>
2019-11-01 13:25:54 +0100

    Uitwerking van de exercise

--- multiplier.php
+++ multiplier.php
@@ -1,8 +1,11 @@
 #!/usr/bin/env php
 <?php
 
-$Complex = 5 - 3;
+$eerste_getal = readline("Wat is jouw eerste nummer?: ");
+$tweede_getal = readline ("Wat is jou tweede nummer?: ");
+$sum3 = (int)$eerste_getal * (int)$tweede_getal;
 
-echo $Complex;
+
+echo "Het antwoord is: $sum3 ($eerste_getal * $tweede_getal)";
     
 ?>
